12f31b449d6158db6b1820b40571404e4f2f63d9
Dhiogo Correa <dhiogo.correa@itera.com.br>
2023-09-23 18:43:13 -0300

    last commit

---
+++ ocr.py
@@ -0,0 +1,8 @@
+import os
+
+def pdf_to_text(file_path: str):
+    pass
+
+
+if __name__ == '__main__':
+    pdf_to_text('pdfs_test/CBSP - CERTIFICADO - ADAILSON NASCIMENTO CARVALHO - 02.08.2019.pdf')
